2ecb474ca6301fe69982823054e127211014e6d2
Ilie Babcenco <ilie.babcenco@gmail.com>
2021-04-14 17:25:41 +0300

    Add associations for friends with each user

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -1,6 +1,7 @@
 class FriendsController < ApplicationController
   before_action :set_friend, only: %i[ show edit update destroy ]
-
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :correct_user, only: [:edit, :update, :destroy]
   # GET /friends or /friends.json
   def index
     @friends = Friend.all
@@ -12,7 +13,8 @@ class FriendsController < ApplicationController
 
   # GET /friends/new
   def new
-    @friend = Friend.new
+    #@friend = Friend.new
+    @friend = current_user.friends.build
   end
 
   # GET /friends/1/edit
@@ -21,8 +23,8 @@ class FriendsController < ApplicationController
 
   # POST /friends or /friends.json
   def create
-    @friend = Friend.new(friend_params)
-
+    #@friend = Friend.new(friend_params)
+    @friend = current_user.friends.build(friend_params)
     respond_to do |format|
       if @friend.save
         format.html { redirect_to @friend, notice: "Friend was successfully created." }
@@ -56,6 +58,11 @@ class FriendsController < ApplicationController
     end
   end
 
+  def correct_user
+    @friend = current_user.friends.find_by(id: params[:id])
+    redirect_to friends_path, notice: "Not Authorized to edit this friend" if @friend.nil?
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_friend
@@ -64,6 +71,6 @@ class FriendsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def friend_params
-      params.require(:friend).permit(:first_name, :last_name, :email, :phone, :github)
+      params.require(:friend).permit(:first_name, :last_name, :email, :phone, :github, :user_id)
     end
 end
